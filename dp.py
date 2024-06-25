420e6113c19a8421621bc8ba33e6ba22263b1e76
ian <141902143+yumosx@users.noreply.github.com>
2024-06-25 11:41:15 +0800

    Update dp.py

--- dp.py
+++ dp.py
@@ -2,4 +2,4 @@ for i in range(1, n):
   for j in range(0, n):
     f[i][j] = f[i-1][j]
       if j >= v[i]:
-        f[i][j] = max(f[i-1][j], f[i-1][j-v[i]] + w[i])
+        f[i][j] = max(f[i][j], f[i-1][j-v[i]] + w[i])
