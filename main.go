02b6ae6f5313ef2d982e0f3ee834ab41c75ebd22
ian <141902143+yumosx@users.noreply.github.com>
2024-12-02 16:56:20 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+//整理一份go语言的模板
+intMin := math.MinInt32
+intMax := math.MaxInt32
+
+//hash的使用方法
+hash := make(map[byte]byte, l)
+if value, ok := hash[value - "a"]; ok {
+  if value != xxx {
+    //条件判断
+  }
+}
